9136b98bb11518390962c68d9941681bc818fea0
MartinezFabian <FabianMartinezDev@gmail.com>
2023-12-04 18:11:38 -0300

    Routing en Express

--- src/app.js
+++ src/app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hola mundo desde Express');
+const products = [
+  { id: 1, name: 'Producto 1', price: 20 },
+  { id: 2, name: 'Producto 2', price: 30 },
+  { id: 3, name: 'Producto 3', price: 40 },
+];
+
+app.get('/products', (req, res) => {
+  res.json(products);
 });
 
 app.listen(port, () => {
